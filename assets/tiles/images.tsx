7745044696a1aaa82568d8cf7d14b7fdcd56d922
Lars Johansson <lars.johansson@ftrack.com>
2023-12-09 21:28:25 +0100

    More updates

--- assets/tiles/images.tsx
+++ assets/tiles/images.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Images" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Images" tilewidth="2542" tileheight="1589" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="2542" height="1589" source="../images/kitchen new.jpg"/>
+ </tile>
+</tileset>
